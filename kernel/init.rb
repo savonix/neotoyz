2ec6ddcedd6ab7edfaba674754bf0f3f3e91188a
Albert Lash <albert.lash@docunext.com>
2008-07-08 10:59:02 +0000

    haven't used ruby in a long while

---
+++ kernel/init.rb
@@ -0,0 +1,20 @@
+module Init
+# Copyright Savonix Corporation
+# Author Albert Lash
+
+class Init
+    
+    # starts the timer
+    def start
+        
+        
+    end
+    
+    def process_gate
+        
+    end
+    
+    def stop
+    end
+
+end
