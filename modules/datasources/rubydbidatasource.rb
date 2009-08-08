4bc35bd1123e0d7e67c693b9abfad64a01de6aa3
Albert Lash <albert.lash@docunext.com>
2009-08-08 03:08:46 +0000

    added some DBI code to datasource

--- modules/datasources/rubydbidatasource.rb
+++ modules/datasources/rubydbidatasource.rb
@@ -1,2 +1,27 @@
 # Copyright Savonix Corporation
 # Author Albert Lash
+require 'dbi'
+
+begin
+    # connect to the MySQL server
+    dbh = DBI.connect("DBI:Mysql:"+$dbconfig['database']+":"+$dbconfig['hostname'], $dbconfig["username"], $dbconfig['password'])
+rescue DBI::DatabaseError => e
+    puts "An error occurred"
+    puts "Error code: #{e.err}"
+    puts "Error message: #{e.errstr}"
+end
+
+sth = dbh.prepare(sql)
+
+begin
+    sth.execute()
+    sth.fetch do |row|
+
+    end
+rescue DBI::DatabaseError => e
+    puts "An error occurred"
+    puts "Error code: #{e.err}"
+    puts "Error message: #{e.errstr}"
+    dbh.disconnect if dbh
+end
+
