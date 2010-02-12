734363e7910d6312d8c404fa1afda2e1810d0b4e
Docunext <albert.lash@docunext.com>
2010-02-12 15:43:48 -0500

    changing indents

--- loader.rb
+++ loader.rb
@@ -15,76 +15,76 @@ sitemap  = ""
 
 FCGI.each_cgi {|cgi|
 
-    if @@init == 0
-        @@app_name = ENV['app_name']
-        @@loc_conf = ENV['loc_conf']
-        @@app_conf = ENV['app_conf']
-        if @@loc_conf == nil
-            @@loc_conf = cgi.env_table['loc_conf']
-            @@app_name = cgi.env_table['app_name']
-        end
-
-        begin
-            config   = XmlSimple.xml_in(@@loc_conf,'ForceArray'=>false)
-            sitemap  = config['build']['sitemap']
-            gate_key = config['build']['query']
-            myfence    = Fence.new(sitemap)
-        rescue StandardError
-            puts cgi.header("text/plain")
-            puts "Config error"
-            puts config
-        end
+  if @@init == 0
+    @@app_name = ENV['app_name']
+    @@loc_conf = ENV['loc_conf']
+    @@app_conf = ENV['app_conf']
+    if @@loc_conf == nil
+      @@loc_conf = cgi.env_table['loc_conf']
+      @@app_name = cgi.env_table['app_name']
     end
 
     begin
-        gate = cgi[gate_key]
+      config   = XmlSimple.xml_in(@@loc_conf,'ForceArray'=>false)
+      sitemap  = config['build']['sitemap']
+      gate_key = config['build']['query']
+      myfence  = Fence.new(sitemap)
     rescue StandardError
-        puts cgi.header("text/plain")
-        puts "Error 2"
+      puts cgi.header("text/plain")
+      puts "Config error"
+      puts config
     end
+  end
+
+  begin
+    gate = cgi[gate_key]
+  rescue StandardError
+    puts cgi.header("text/plain")
+    puts "Error 2"
+  end
 
 
-    if gate == 'x-dynamic-css'
-        puts cgi.header("text/css")
+  if gate == 'x-dynamic-css'
+    puts cgi.header("text/css")
+  else
+    if @@app_name == 'pbooks'
+      puts cgi.header("application/xhtml+xml")
     else
-        if @@app_name == 'pbooks'
-            puts cgi.header("application/xhtml+xml")
-        else
-            puts cgi.header("text/plain")
-        end
+      puts cgi.header("text/plain")
     end
+  end
 
+  begin
+    Init.start(cgi)
+    duration = 0 #Init.stop
+    output   = Init.display(gate,@@app_name)
+  rescue StandardError => e
+    puts e
+    puts myxsl
+    puts "Error 123"
+  end
+
+  if gate == 'x-dynamic-css'
+    puts output
+  else
     begin
-        Init.start(cgi)
-        duration = 0 #Init.stop
-        output   = Init.display(gate,@@app_name)
-    rescue StandardError => e
-        puts e
-        puts myxsl
-        puts "Error 123"
+      @@blah = @@blah + 1
+      output = output.gsub("</body>", "")
+      output = output.gsub("</html>", "")
+      puts output
+      puts config['build']['sitemap']
+      puts '<br/><br/>Request duration:'
+      puts @@blah
+      puts @@init
+      dur = duration * 1000
+      puts dur
+      puts '</body></html>'
+    rescue
+      puts 'error'
     end
+  end
 
-    if gate == 'x-dynamic-css'
-        puts output
-    else
-        begin
-            @@blah = @@blah + 1
-            output = output.gsub("</body>", "")
-            output = output.gsub("</html>", "")
-            puts output
-            puts config['build']['sitemap']
-            puts '<br/><br/>Request duration:'
-            puts @@blah
-            puts @@init
-            dur = duration * 1000
-            puts dur
-            puts '</body></html>'
-        rescue
-            puts 'error'
-        end
-    end
-
-    if @@init == 0
-        @@init = 1
-    end
+  if @@init == 0
+    @@init = 1
+  end
 }
--- modules/datasources/rubydbidatasource.rb
+++ modules/datasources/rubydbidatasource.rb
@@ -3,25 +3,25 @@
 require 'dbi'
 
 begin
-    # connect to the MySQL server
-    dbh = DBI.connect("DBI:Mysql:"+$dbconfig['database']+":"+$dbconfig['hostname'], $dbconfig["username"], $dbconfig['password'])
+  # connect to the MySQL server
+  dbh = DBI.connect("DBI:Mysql:"+$dbconfig['database']+":"+$dbconfig['hostname'], $dbconfig["username"], $dbconfig['password'])
 rescue DBI::DatabaseError => e
-    puts "An error occurred"
-    puts "Error code: #{e.err}"
-    puts "Error message: #{e.errstr}"
+  puts "An error occurred"
+  puts "Error code: #{e.err}"
+  puts "Error message: #{e.errstr}"
 end
 
 sth = dbh.prepare(sql)
 
 begin
-    sth.execute()
-    sth.fetch do |row|
+  sth.execute()
+  sth.fetch do |row|
 
-    end
+  end
 rescue DBI::DatabaseError => e
-    puts "An error occurred"
-    puts "Error code: #{e.err}"
-    puts "Error message: #{e.errstr}"
-    dbh.disconnect if dbh
+  puts "An error occurred"
+  puts "Error code: #{e.err}"
+  puts "Error message: #{e.errstr}"
+  dbh.disconnect if dbh
 end
 
